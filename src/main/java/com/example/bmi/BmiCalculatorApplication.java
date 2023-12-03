e4890038811b898bd45837cc99e916447d71329e
oshadhi-sam <oshadhi-gangani.samarasinghe@informatik.hs-fulda.de>
2023-12-03 13:33:13 +0100

    dev : update databaseCofiguration to be read through azure configurations

--- src/main/java/com/example/bmi/BmiCalculatorApplication.java
+++ src/main/java/com/example/bmi/BmiCalculatorApplication.java
@@ -2,8 +2,10 @@ package com.example.bmi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example.config")
 public class BmiCalculatorApplication {
 
 	public static void main(String[] args) {
